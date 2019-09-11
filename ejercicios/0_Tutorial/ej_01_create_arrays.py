a91c880810c4a79751df7cd6abbcd725bd05c78f
Jorge Mauricio <jorge.ernesto.mauricio@gmail.com>
2019-09-11 14:08:36 -0500

    modificar los títulos de cada folder

--- ejercicios/0_Tutorial/ej_01_create_arrays.py
+++ ejercicios/0_Tutorial/ej_01_create_arrays.py
@@ -6,16 +6,16 @@ Created on Mon Jul 17 16:17:25 2017
 @author: jorgemauricio
 """
 
-# librerias
+# librerías
 import numpy as np
 
 # hacer una lista
 arr1 = [1,2,3,4]
 
-# crear el arreglo de numpy
+# crear el arreglo en numpy
 myArr1 = np.array(arr1)
 
-#Imprimir
+# desplegar el arreglo de numpy
 myArr1
 
 # hacer otra lista
@@ -24,19 +24,19 @@ arr2 = [11,22,33,44]
 # hacer una lista de listas
 myList = [arr1,arr2]
 
-# hacer un arreglomultimimencional
+# hacer un arreglo multidimencional
 myArr2 = np.array(myList)
 
 # desplegar arreglo
 myArr2
 
-# obtener la dimension del arreglo
+# obtener la dimensión del arreglo
 myArr2.shape
 
-# desplegar el tipy de datos del arreglo 
+# desplegar el tipo de datos del arreglo
 myArr2.dtype
 
-# hacer arreglos especiales 
+# Arreglos especiales
 # ceros
 np.zeros(5)
 
@@ -47,8 +47,8 @@ np.ones((5,5))
 np.empty(5)
 np.empty((3,4))
 
-# arreglo de identidad
+# matriz de identidad
 np.eye(5)
 
-# Using a range
+# arreglo de un determinado número de elementos
 np.arange(5)
